01d6554a49d6c0f799d28c5da23f1111232c7d2b
ccov <cover3dcall@gmail.com>
2016-05-11 19:52:25 +0700

    Protect main code on Windows

--- test_string_matching.py
+++ test_string_matching.py
@@ -3,32 +3,35 @@ from time import time
 from string_matching import *
 import pickle
 import sys
+from multiprocessing import freeze_support
 
 string = sys.argv[1]
 
-print "trying with " + string
-
-print "checking if it is exactly in list..."
-tt = time()
-try:
-	if string in bands:
-		print "found exactly"
-except:
-	print "not found exactly"
-print 'trying list look-up took ' + str(time()-tt)
-print "\n"
-
-print "searching through entire list..."
-tt = time()
-result = searchThroughList(string)
-print "Best match: " + result
-print 'searching through entire list took ' + str(time()-tt)
-print "\n"
-
-
-print "searching through hashed list..."
-tt = time()
-result = searchThroughHash(string)
-print "Best match: " + result
-print 'searching through hashed list took ' + str(time()-tt)
+if __name__ == '__main__':
+  freeze_support()
+  print "trying with " + string
+  
+  print "checking if it is exactly in list..."
+  tt = time()
+  try:
+  	if string in bands:
+  		print "found exactly"
+  except:
+  	print "not found exactly"
+  print 'trying list look-up took ' + str(time()-tt)
+  print "\n"
+  
+  print "searching through entire list..."
+  tt = time()
+  result = searchThroughList(string)
+  print "Best match: " + result
+  print 'searching through entire list took ' + str(time()-tt)
+  print "\n"
+  
+  
+  print "searching through hashed list..."
+  tt = time()
+  result = searchThroughHash(string)
+  print "Best match: " + result
+  print 'searching through hashed list took ' + str(time()-tt)
 
